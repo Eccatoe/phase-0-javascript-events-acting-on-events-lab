31df3cc97671a1fb39955117209ac8852a9d32a3
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-15 20:10:03 -0500

    commit tree 1e93a1cc4cf9c8b901b4bc14659cffac1a88a60d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger=document.getElementById("dodger");
+const game=document.getElementById("game");
+dodger.style.backgroundColor="#0066dd";
+
+const moveDodger=document.addEventListener('keydown', (event)=>{
+        const dodgerLeftNumbers=dodger.style.left.replace("px", "");
+        const left=parseInt(dodgerLeftNumbers, 10);
+        const dodgerBottomNumbers=dodger.style.bottom.replace("px", "")
+        const bottom=parseInt(dodgerBottomNumbers, 10)
+        if (event.key==="ArrowLeft" && left>0){
+        return dodger.style.left=`${left-10}px`
+    } else if (event.key==="ArrowRight" && left<360){
+        return dodger.style.left=`${left+10}px`
+    } else if (event.key==='ArrowUp' && bottom<380){
+        return dodger.style.bottom=`${bottom+10}px`
+    } else if (event.key==="ArrowDown" && bottom >0){
+        return dodger.style.bottom=`${bottom-10}px`
+    }
+})
+
+
+
+
+
+
+
